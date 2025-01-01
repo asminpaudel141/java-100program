f65119c86f7e1e9c68c19003881166dd4291e4d1
student <ganeshpaudel141@gmail.com>
2025-01-01 19:28:07 +0545

    Display odd numbers between 1 -100

---
+++ odd_number.java
@@ -0,0 +1,10 @@
+public class odd_number {
+    public static void main(String[] args){
+        System.out.println("The odd number are");
+        for(int i=0;i<100;i++){
+            if (i%2!=0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
