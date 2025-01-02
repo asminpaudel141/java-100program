ae1c159af9531537711edb031bc96166201d3b2b
student <ganeshpaudel141@gmail.com>
2025-01-02 17:43:30 +0545

    Find sum of first n numbers

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # java-100program
 
 # 1. Display odd numbers between 1 -100 
+# 2. Sum of odd numbers between 1 -100
+# 3. Total number of odd numbers between 1 -100
---
+++ SumOfNum.java
@@ -0,0 +1,10 @@
+public class SumOfNum {
+    public static void main(String[] args){
+        int sum=0;
+        int n=10;
+        for(int i=0;i<n;i++){
+            sum+=i;
+        }
+        System.out.println("sum of n number is"+sum);
+    }
+}
