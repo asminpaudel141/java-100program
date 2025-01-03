6456e02036edc18b4df60b231f5ff7ea608c77de
asmin <asminpaudel141@gmail.com>
2025-01-03 14:37:12 +0545

    Java program to find Armstrong number

---
+++ ArmstrongNumber.java
@@ -0,0 +1,20 @@
+public class ArmstrongNumber {
+    public static void main(String[] args){
+        int n, original, sum=0, r;
+        n=1534;
+        original=n;
+        for(int i=0;i<n;i++){
+            while (n>0) {
+                r=n%10;
+                sum=sum+(r*r*r);
+                n=n/10;
+            }
+        }
+        if(original==sum) {
+            System.out.println("Given number is armStrong number "+original);
+            
+        }else{
+            System.out.println("Given number is not a armStrong number "+original);
+        }
+    }
+}
