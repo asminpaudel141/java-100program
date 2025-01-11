55496acadfb703b7ec2845c1ac14ad414a11867f
asmin <asminpaudel141@gmail.com>
2025-01-11 20:21:04 +0545

    Find the index of the largest number in an array

---
+++ LargestNumberIndex.java
@@ -0,0 +1,14 @@
+public class LargestNumberIndex {
+    public static void main(String[] args) {
+        int nums[]={5,34,78,3,45,1,99,23};
+        int index=0;
+        int largest=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if (largest<nums[i]) {
+                largest=nums[i];
+                index=i;
+            }
+        }
+        System.out.println("The largest number index is : "+index);
+    }
+}
