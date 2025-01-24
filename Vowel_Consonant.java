9aa2d47f6e6272fbda0a64def1417fd60c566a9c
asmin <asminpaudel141@gmail.com>
2025-01-24 22:10:03 +0545

    Check Vowel or Consonant in Java

---
+++ Vowel_Consonant.java
@@ -0,0 +1,10 @@
+public class Vowel_Consonant {
+    public static void main(String[] args){
+        char ch='z';
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+            System.out.println("vowel "+ch);
+        }else {
+            System.out.println("consonant "+ch);
+        }
+    }
+}
