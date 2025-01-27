d3611a094ba08aa5eaf74fea37960cb7bac467bd
asmin <asminpaudel141@gmail.com>
2025-01-27 19:45:21 +0545

    Find Maximum of Three Numbers

---
+++ find_maximum.java
@@ -0,0 +1,14 @@
+public class find_maximum {
+    public static void main(String[] args){
+    int a=80;
+    int b=20;
+    int c=40;
+    if(a<b){
+        System.out.println(b);
+    }else if (a<c){
+        System.out.println(c);
+    }else {
+        System.out.println(a);
+    }
+}
+}
