18c16ab622ad4ae42f40ed347aa2db84e4960dc2
asmin <asminpaudel141@gmail.com>
2025-01-31 22:48:04 +0545

    Multiply Two Numbers in Java

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,8 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args){
+       double number1=2.5;
+       double number2=3.5;
+       double multipe=number1*number2;
+       System.out.println(multipe);
+    }
+}
