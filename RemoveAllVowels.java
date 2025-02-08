7621c4ec887c13802b0afa9f2f68d6b89ffeb7e5
asmin <asminpaudel141@gmail.com>
2025-02-08 23:11:08 +0545

    new

--- RemoveAllVowels.java
+++ RemoveAllVowels.java
@@ -1,7 +1,7 @@
 public class RemoveAllVowels {
 
     public static void main(String[] args) {
-    String string = "Welcome to Candid Java Programming";
+    String string = "Welcome to  Candid Java Programming";
     System.out.println("Input String : "+string);
     string = string.replaceAll("[AaEeIiOoUu]", "");
     System.out.println(string);
