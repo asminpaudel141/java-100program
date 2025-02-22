b714c61094000c5179d4be19c1a6fa829b9d81d9
asmin <asminpaudel141@gmail.com>
2025-02-22 19:37:42 +0545

    new

---
+++ Dog.java
@@ -0,0 +1,21 @@
+class Dog{
+    public void bark(){
+        System.out.println("woof");
+    }
+}
+
+ class pug extends Dog {
+    public void sniff(){
+        System.out.println("sniff");
+    }
+    public void bark(){
+        System.out.println("bow");
+    }
+    public static void main(String[] args) {
+        Dog ap=new Dog();
+        ap.bark();
+        pug op=new pug();
+        op.bark();
+        op.sniff();
+    }
+}
