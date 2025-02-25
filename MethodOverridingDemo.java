b01b3391da13d1c57813628af769ac1a8b399644
asmin <asminpaudel141@gmail.com>
2025-02-25 21:26:45 +0545

    Write a Java program to demonstrate method overriding.

---
+++ MethodOverridingDemo.java
@@ -0,0 +1,37 @@
+class Animal{
+    // method to be override
+    void makeSound(){
+        System.out.println("Animal makes sound");
+    }
+}
+
+    // subclass overriding the method
+class Dog  extends Animal{
+    @Override
+    void makeSound(){
+        System.out.println("Dog barks");
+    }
+}    
+
+// Another subclass overriding the method
+class Cat extends Animal {
+    @Override
+    void makeSound(){
+        System.out.println("cat meowa");
+    }    
+}
+
+// main class to test override
+public class MethodOverridingDemo {
+    public static void main(String[] args) {
+    Animal myAnimal=new Animal();
+    myAnimal.makeSound();
+
+    Dog myDog=new Dog();
+    myDog.makeSound();
+
+    Cat myCat=new Cat();
+    myCat.makeSound();
+
+    }
+}
