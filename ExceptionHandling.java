013c5da91417023fb3a9561c05afff96e58e4c82
asmin <asminpaudel141@gmail.com>
2025-08-01 19:57:21 +0545

    new

---
+++ ExceptionHandling.java
@@ -0,0 +1,11 @@
+import java.lang.Thread.State;
+
+class ExceptionHandling{
+    public static void main(String[] args){
+        try{
+            int number= 10/0;
+        }catch(ArithmeticException e){
+            System.out.println("cannot divide by Zero");
+        }
+    }
+}
