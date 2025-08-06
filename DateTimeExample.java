59373796972a4c4b3725f2f2c919fee1296d7f6b
asmin <asminpaudel141@gmail.com>
2025-08-06 20:47:11 +0545

    How to print date and time in Java Program

---
+++ DateTimeExample.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class DateTimeExample{
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("current time is :"+ date.toString());
+    }
+}
+
