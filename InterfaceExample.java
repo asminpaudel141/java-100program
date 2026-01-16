30791761e3b3c93880db731e661dee3e9f6eabe3
Asmin Paudel <asminpaudel141@gmail.com>
2026-01-16 23:32:46 +0545

    InterfaceExample

--- InterfaceExample.java
+++ InterfaceExample.java
@@ -1,16 +1,16 @@
-interface  Animal {
+interface Animal {
     void sound();
 }
-class Dog implements Animal  {
-    public void sound(){
-        System.out.println("Dog barks ");
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog barks");
     }
- }
+}
 
 public class InterfaceExample {
     public static void main(String[] args) {
-        Dog d=new Dog();
+        Dog d = new Dog();
         d.sound();
     }
-    
 }
