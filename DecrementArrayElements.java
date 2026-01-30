a59eb45abef9af6360d1e610215ca21c16a0de68
Asmin Paudel <asminpaudel141@gmail.com>
2026-01-30 23:50:00 +0545

    Write a Java program to decrement each element of an integer array by 1.

---
+++ DecrementArrayElements.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class DecrementArrayElements {
+
+    public static int[] decrementArrayElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] - 1;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 3, 8};
+
+        int[] result = decrementArrayElements(arr);
+
+        System.out.println("Array after decrementing each element:");
+        System.out.println(Arrays.toString(result));
+    }
+}
